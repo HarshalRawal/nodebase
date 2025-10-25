d31cba7b9f3f7a34bd1f6c230de54fed329e74b7
HarshalRawal <harshalrawal463@gmail.com>
2025-10-25 22:13:30 +0530

    db set-up

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "./generated/prisma/client";
+
+const golbalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = golbalForPrisma.prisma || new PrismaClient();
+
+if(process.env.NODE_ENV !== "production") golbalForPrisma.prisma = prisma;  
+
+
+export default prisma
