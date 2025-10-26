3f741d0df889d7287bae02d8d07cdc849cfe03a2
HarshalRawal <harshalrawal463@gmail.com>
2025-10-26 12:15:45 +0530

    trpc setup with prefetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { getQueryClient,trpc } from "./trpc/server"
+import { Client } from "./client"
+import { HydrationBoundary,dehydrate } from "@tanstack/react-query"
+import { queryObjects } from "v8"
+const page = async() => {
+  const queryClient = getQueryClient()
 
-const page = () => {
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"harshal"}))
   return (
-    <div className='text-red-500'>page</div>
+    <div className="min-h-screen min-w-screen items-center justify-center">
+     <HydrationBoundary state={dehydrate(queryClient)}>
+      <Client />
+     </HydrationBoundary>
+    </div>
   )
 }
 
